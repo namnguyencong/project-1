30c11e1ae2ae1e5dcd854a4e8d5f6b8ac509414c
nnguyen17 <nnguyen17@hawk.iit.edu>
2020-02-26 20:58:09 -0600

    declaring variables

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-#js
+function init() {
+    var button = document.getElementById('submit');
+
+    function Alert() {
+        var fname = document.forms["myForm"]["fname_field"].value;
+		var lname = document.forms["myForm"]["lname_field"].value;
+		var email = document.forms["myForm"]["email_field"].value;
+		var phone = document.forms["myForm"]["tel_field"].value;
+		var dob = document.forms["myForm"]["date_field"].value;
+		var uname = document.forms["myForm"]["username_field"].value;
+		var password = document.forms["myForm"]["password_field"].value;
+		var repassword = document.forms["myForm"]["rpassword_field"].value;
+		
